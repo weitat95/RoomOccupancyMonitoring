2718d33961c9c915f71dabf2eaa25021207ab4e0
Wei Tat Lee <weitat95@live.com>
2019-02-14 19:39:30 +0000

    Mbed bluetooth buttons

--- main.cpp
+++ main.cpp
@@ -1,98 +1,126 @@
-#include <events/mbed_events.h>
-#include <mbed.h>
+/* mbed Microcontroller Library
+ *  * Copyright (c) 2006-2013 ARM Limited
+ *   *
+ *    * Licensed under the Apache License, Version 2.0 (the "License");
+ *     * you may not use this file except in compliance with the License.
+ *      * You may obtain a copy of the License at
+ *       *
+ *        *     http://www.apache.org/licenses/LICENSE-2.0
+ *         *
+ *          * Unless required by applicable law or agreed to in writing, software
+ *           * distributed under the License is distributed on an "AS IS" BASIS,
+ *            * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *             * See the License for the specific language governing permissions and
+ *              * limitations under the License.
+ *               */
+
+#include "mbed.h"
 #include "ble/BLE.h"
-#include "ble/Gap.h"
-#include "ble/services/HeartRateService.h"
-#include "main.h"
+#include "ButtonService.h"
 
-DigitalOut led1(LED1);
-DigitalOut led2(LED2);
-DigitalOut led3(LED3);
-DigitalOut led4(LED4);
-Serial PC(USBTX, USBRX);
+DigitalOut  led1(LED1);
+InterruptIn button(BUTTON1);
 
+const static char     DEVICE_NAME[] = "Button";
+static const uint16_t uuid16_list[] = {ButtonService::BUTTON_SERVICE_UUID};
 
-const static char     DEVICE_NAME[] = "Not a Spy Drone";
+enum {
+    RELEASED = 0,
+    PRESSED,
+    IDLE
+};
+static uint8_t buttonState = IDLE;
 
-static EventQueue eventQueue(/* event count */ 16 * EVENTS_EVENT_SIZE);
+static ButtonService *buttonServicePtr;
 
-void onBleInitError(BLE &ble, ble_error_t error)
+void buttonPressedCallback(void)
 {
-    (void)ble;
-    (void)error;
-   /* Initialization error handling should go here */
-   PC.printf("Bluetooth initialisation failed");
-   led1=0;
+    /* Note that the buttonPressedCallback() executes in interrupt context, so it is safer to access
+ *      * BLE device API from the main thread. */
+    buttonState = PRESSED;
 }
 
-// Initiialization routine goes here
+void buttonReleasedCallback(void)
+{
+    /* Note that the buttonReleasedCallback() executes in interrupt context, so it is safer to access
+ *      * BLE device API from the main thread. */
+    buttonState = RELEASED;
+}
+
+void disconnectionCallback(const Gap::DisconnectionCallbackParams_t *params)
+{
+    BLE::Instance().gap().startAdvertising();
+}
+
+void periodicCallback(void)
+{
+    led1 = !led1; /* Do blinky on LED1 to indicate system aliveness. */
+}
+
+/**
+ *  * This function is called when the ble initialization process has failled
+ *   */
+void onBleInitError(BLE &ble, ble_error_t error)
+{
+    /* Initialization error handling should go here */
+}
+
+/**
+ *  * Callback triggered when the ble initialization process has finished
+ *   */
 void bleInitComplete(BLE::InitializationCompleteCallbackContext *params)
 {
-
     BLE&        ble   = params->ble;
     ble_error_t error = params->error;
 
-    // healthy error checking
     if (error != BLE_ERROR_NONE) {
+        /* In case of error, forward the error handling to onBleInitError */
         onBleInitError(ble, error);
         return;
     }
 
-    if (ble.getInstanceID() != BLE::DEFAULT_INSTANCE) {
+    /* Ensure that it is the default instance of BLE */
+    if(ble.getInstanceID() != BLE::DEFAULT_INSTANCE) {
         return;
     }
 
+    ble.gap().onDisconnection(disconnectionCallback);
 
-/* Setup advertising. */
-    ble.gap().accumulateAdvertisingPayload(GapAdvertisingData::GENERIC_HEART_RATE_SENSOR);
-    ble.gap().accumulateAdvertisingPayload(GapAdvertisingData::COMPLETE_LOCAL_NAME,
-       (uint8_t*)DEVICE_NAME, sizeof(DEVICE_NAME));
+    /* Setup primary service */
+    buttonServicePtr = new ButtonService(ble, false /* initial value for button pressed */);
+
+    /* setup advertising */
+    ble.gap().accumulateAdvertisingPayload(GapAdvertisingData::BREDR_NOT_SUPPORTED | GapAdvertisingData::LE_GENERAL_DISCOVERABLE);
+    ble.gap().accumulateAdvertisingPayload(GapAdvertisingData::COMPLETE_LIST_16BIT_SERVICE_IDS, (uint8_t *)uuid16_list, sizeof(uuid16_list));
+    ble.gap().accumulateAdvertisingPayload(GapAdvertisingData::COMPLETE_LOCAL_NAME, (uint8_t *)DEVICE_NAME, sizeof(DEVICE_NAME));
     ble.gap().setAdvertisingType(GapAdvertisingParams::ADV_CONNECTABLE_UNDIRECTED);
-    ble.gap().setAdvertisingInterval(1000); /* 1000ms */
+    ble.gap().setAdvertisingInterval(1000); /* 1000ms. */
     ble.gap().startAdvertising();
+
 }
 
-// events processing handler
-void scheduleBleEventsProcessing(BLE::OnEventsToProcessCallbackContext* context) {
-    eventQueue.call(callback(&(context->ble), &BLE::processEvents));
-}
-
-int main()
+int main(void)
 {
-    // create BLE instance
+    led1 = 1;
+    Ticker ticker;
+    ticker.attach(periodicCallback, 1);
+    button.fall(buttonPressedCallback);
+    button.rise(buttonReleasedCallback);
+
     BLE &ble = BLE::Instance();
-    ble.onEventsToProcess(scheduleBleEventsProcessing);
     ble.init(bleInitComplete);
+    
+    /* SpinWait for initialization to complete. This is necessary because the
+ *      * BLE object is used in the main loop below. */
+    while (ble.hasInitialized()  == false) { /* spin loop */ }
+    
+    while (true) {
+        if (buttonState != IDLE) {
+            buttonServicePtr->updateButtonState(buttonState);
+            buttonState = IDLE;
+        }
 
-    // link to events processing handler
-    // initialise the BLE device
-    led1=1;
-    led2=1;
-    led3=0;
-    led4=1;
-    PC.baud(115200);
-    PC.printf("Initialising Buttons");
-    init_buttons();
-    init_leds();
-
-    while(0) {
-      PC.printf("Nice\n");
-      led4=0;
-
-      wait_ms(500);
-
-      led4=1;
-
-      wait_ms(500);
-
-      // If button 1 is pressed turn on LED 1
-      if((NRF_GPIO->DIR & (1 << 17))>> 17) {
-        led1=1;
-        break;
-      }
+        ble.waitForEvent();
     }
-
-    eventQueue.dispatch_forever(); // blocking
-
-    return 0;
 }
+
